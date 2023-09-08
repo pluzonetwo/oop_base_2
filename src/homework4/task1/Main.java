e7b8eec83bac9c2e58cbd5e6b5d94aa4860700b4
Konstantin Os <permkonstantin@gmail.com>
2023-09-08 16:36:24 +0500

    hw4_task1_ready

---
+++ src/homework4/task1/Main.java
@@ -0,0 +1,11 @@
+package homework4.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        PrintElement printEl = new PrintElement();
+
+        printEl.printElement(5);
+        printEl.printElement("Hello world!");
+        printEl.printElement(13.5);
+    }
+}
---
+++ src/homework4/task1/PrintElement.java
@@ -0,0 +1,7 @@
+package homework4.task1;
+
+public class PrintElement {
+    public <T> void printElement(T param) {
+        System.out.println(param);
+    }
+}
